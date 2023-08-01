2613e941cc3fc9b9aaf0d70cb447e01a41bc39da
qianniancao <grassedge@outlook.com>
2023-08-01 17:00:39 +0800

    IGraph基本完成

--- SharpGraphT/IGraphIterables.cs
+++ SharpGraphT/IGraphIterables.cs
@@ -1,6 +1,14 @@
 ﻿namespace SharpGraphT
 {
+    /// <summary>
+    /// 将图表示为视图集合，适用于包含非常多顶点或边的图。使用这些方法编写的图算法可以使用不受32位算术限制的图。
+    /// </summary>
+    /// <typeparam name="TV"></typeparam>
+    /// <typeparam name="TE"></typeparam>
     public interface IGraphIterables<TV, TE> where TE : class, new()
     {
+        IGraph<TV, TE> Graph { get; }
+        IEnumerable Edges => new LiveEnumerableWrapper<TE>(Graph.EdgeSet);
+        
     }
 }
--- SharpGraphT/IGraphType.cs
+++ SharpGraphT/IGraphType.cs
@@ -1,6 +1,24 @@
 ﻿namespace SharpGraphT
 {
-    public class GraphType
+    public interface IGraphType
     {
+        bool IsDirected { get; }
+        bool IsUndirected { get; }
+        bool IsMixed { get; }
+        bool IsAllowingMultipleEdges { get; }
+        bool IsAllowingSelfLoops { get; }
+        bool IsAllowingCycles { get; }
+        bool IsWeighted { get; }
+        bool IsSimple { get; }
+        bool IsPseudograph { get; }
+        bool IsMultigraph { get; }
+        bool IsModifiable { get; }
+        IGraphType AsDirected();
+        IGraphType AsUndirected();
+        IGraphType AsMixed();
+        IGraphType AsUnweighted();
+        IGraphType AsWeighted();
+        IGraphType AsModifiable();
+        IGraphType AsUnmodifiable();
     }
 }
