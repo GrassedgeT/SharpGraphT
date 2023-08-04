4f55a64d6af5602e576ad7c5bb2be0b9371344f7
qianniancao <grassedge@outlook.com>
2023-08-04 17:32:57 +0800

    初步完成

--- SharpGraphT.Core/Graph/DefaultGraphIterables.cs
+++ SharpGraphT.Core/Graph/DefaultGraphIterables.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpGraphT.Utils.Sharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,23 @@ using System.Threading.Tasks;
 
 namespace SharpGraphT.Core.Graph;
 
+/// <summary>
+/// 图可迭代的默认实现，简单地委托给集合实现。
+/// </summary>
+/// <typeparam name="TV"></typeparam>
+/// <typeparam name="TE"></typeparam>
 public class DefaultGraphIterables<TV, TE> : IGraphIterables<TV, TE>
     where TE : class, new()
 {
+    protected IGraph<TV, TE>? graph;
 
+    public IGraph<TV, TE> GetGraph() => graph;
+
+    public DefaultGraphIterables() : this(null) { }
+
+    public DefaultGraphIterables(IGraph<TV, TE>? graph)
+    {
+        this.graph = Objects.RequireNonNull(graph);
+    }
 }
 
