f68eed11a0ccd61523d6f1772790d58020b8851a
qianniancao <grassedge@outlook.com>
2023-08-04 16:54:35 +0800

    初步完成

--- SharpGraphT.Core/IGraphIterables.cs
+++ SharpGraphT.Core/IGraphIterables.cs
@@ -1,5 +1,4 @@
 ﻿using SharpGraphT.Util;
-using System.Collections;
 
 namespace SharpGraphT
 {
@@ -11,7 +10,34 @@ namespace SharpGraphT
     public interface IGraphIterables<TV, TE> where TE : class, new()
     {
         IGraph<TV, TE> Graph { get; }
-        IEnumerable Edges => new LiveEnumerableWrapper<TE>(Graph.EdgeSet);
         
+        IEnumerable<TE> Edges => 
+            new LiveEnumerableWrapper<TE>(() => Graph.EdgeSet());
+        
+        long EdgeCount => Graph.EdgeSet().Count;
+
+        IEnumerable<TV> Vertices => 
+            new LiveEnumerableWrapper<TV>(() => Graph.VertexSet());
+
+        long VertexCount => Graph.VertexSet().Count;
+
+        IEnumerable<TE> EdgesOf(TV vertex) => 
+            new LiveEnumerableWrapper<TE>(() => Graph.EdgesOf(vertex));
+
+        long DegreeOf(TV vertex) => Graph.DegreeOf(vertex);
+
+        IEnumerable<TE> IncomingEdgesOf(TV vertex) => 
+            new LiveEnumerableWrapper<TE>(() => Graph.IncomingEdgesOf(vertex));   
+
+        long InDegreeOf(TV vertex) => Graph.InDegreeOf(vertex);
+
+        IEnumerable<TE> OutgoingEdgesOf(TV vertex) => 
+            new LiveEnumerableWrapper<TE>(() => Graph.OutgoingEdgesOf(vertex));
+
+        long OutDegreeOf(TV vertex) => Graph.OutDegreeOf(vertex);
+
+        IEnumerable<TE> allEdges(TV sourceVertex, TV targetVertex) => 
+            new LiveEnumerableWrapper<TE>(() => 
+            Graph.GetAllEdges(sourceVertex, targetVertex));
     }
 }
