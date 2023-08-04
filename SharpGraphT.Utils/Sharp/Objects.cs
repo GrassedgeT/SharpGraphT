304cf4cef5968d03b985b8db4e3d83530d299eda
qianniancao <grassedge@outlook.com>
2023-08-04 10:48:03 +0800

    添加RequireNonNull方法

---
+++ SharpGraphT.Utils/Sharp/Objects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGraphT.Utils.Sharp;
+
+public static class Objects
+{
+    //两个用于检查指定的对象引用是否为null的方法。对应仿照java中的Objects.requireNonNull(T obj)方法。
+    public static T RequireNonNull<T>(T obj) where T : class? =>
+        obj ?? throw new ArgumentNullException(nameof(obj));
+    public static T RequireNonNull<T>(T obj, string message) where T : class? =>
+        obj ?? throw new ArgumentNullException(message);
+}
+ 
