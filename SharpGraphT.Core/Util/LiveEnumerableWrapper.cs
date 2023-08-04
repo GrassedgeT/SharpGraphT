bf061c6a48b628c1641ac624deb6e27ff5b1ca3d
qianniancao <grassedge@outlook.com>
2023-08-04 10:46:52 +0800

    初步完成

---
+++ SharpGraphT.Core/Util/LiveEnumerableWrapper.cs
@@ -0,0 +1,33 @@
+﻿using SharpGraphT.Utils.Sharp;
+using System.Collections;
+
+namespace SharpGraphT.Util
+{
+    public class LiveEnumerableWrapper<TE> : IEnumerable<TE> 
+    {
+        private Func<IReadOnlySet<TE>>? _supplier;
+
+        public LiveEnumerableWrapper() : this(null) { }
+
+        public LiveEnumerableWrapper(Func<IReadOnlySet<TE>>? supplier)
+        {
+            _supplier = Objects.RequireNonNull(supplier);
+        }
+
+        public IEnumerator<TE> GetEnumerator()
+        {
+            return _supplier().GetEnumerator();
+        }
+
+        public Func<IReadOnlySet<TE>>? Supplier
+        {
+            get => _supplier;
+            set => _supplier = value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
