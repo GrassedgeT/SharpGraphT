26ec8a50d0fdf801caea2b7d1814fd6bd7121659
qianniancao <grassedge@outlook.com>
2023-08-07 18:48:11 +0800

    新建待完成

---
+++ SharpGraphT.Core/Graph/AsUndirectedGraph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGraphT.Core.Graph;
+[Serializable]
+public class AsUndirectedGraph<TV, TE> : GraphDelegator<TV, TE>, IGraph<TV, TE>
+    where TE : class, new()
+{
+    public AsUndirectedGraph(IGraph<TV, TE> g) : base(g)
+    {
+
+    }
+}
+
