524a99574fd1a91b76931e441801b992ceedecd9
qianniancao <grassedge@outlook.com>
2023-08-07 18:48:22 +0800

    新建待完成

---
+++ SharpGraphT.Core/Graph/GraphDelegator.cs
@@ -0,0 +1,18 @@
+﻿using SharpGraphT.Core.Graph.builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGraphT.Core.Graph
+{
+    public class GraphDelegator<TV, TE> : AbstractGraph<TV, TE>, IGraph<TV, TE>
+        where TE : class, new()
+    {
+        public GraphDelegator(IGraph<TV, TE> g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
