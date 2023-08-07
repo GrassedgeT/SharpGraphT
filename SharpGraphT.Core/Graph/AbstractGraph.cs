f3502402be35bc24da450a2bd08e7a17917b944e
qianniancao <grassedge@outlook.com>
2023-08-07 18:47:57 +0800

    新建待完成

---
+++ SharpGraphT.Core/Graph/AbstractGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGraphT.Core.Graph
+{
+    public abstract class AbstractGraph<TV, TE> : IGraph<TV, TE>
+        where TE : class, new()
+    {
+        
+    }
+}
