031fd452439424837368bee339c1702af6ceac32
qianniancao <grassedge@outlook.com>
2023-08-24 23:11:27 +0800

    初步完成

---
+++ SharpGraphT.Core/Graph/builder/GraphBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGraphT.Core.Graph.builder
+{
+    public class GraphBuilder<TV, TE, TG> : AbstractGraphBuilder<TV, TE, TG, GraphBuilder<TV, TE, TG>>
+         where TG : IGraph<TV, TE>
+         where TE : class, new()
+    {
+        public GraphBuilder(TG baseGraph) : base(baseGraph) { }
+
+        protected override GraphBuilder<TV, TE, TG> Self() => this;
+    }
+}
