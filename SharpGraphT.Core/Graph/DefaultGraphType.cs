0cfe9c4b473a25c6878ec39c1473ed8dca7736e7
qianniancao <grassedge@outlook.com>
2023-08-30 10:02:05 +0800

    正在进行Builder部分

--- SharpGraphT.Core/Graph/DefaultGraphType.cs
+++ SharpGraphT.Core/Graph/DefaultGraphType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -64,6 +65,75 @@ namespace SharpGraphT.Core.Graph
         public IGraphType AsWeighted()
         {
             throw new NotImplementedException();
+        }
+
+        public class Builder
+        {
+            private bool _directed;
+            private bool _undirected;
+            private bool _allowSelfloops;
+            private bool _allowMultipleEdges;
+            private bool _weighted;
+            private bool _allowCycles;
+            private bool _modifiable;
+
+            public Builder()
+            {
+                this._directed = false;
+                this._undirected = true;
+                this._allowSelfloops = true;
+                this._allowMultipleEdges = true;
+                this._weighted = false;
+                this._allowCycles = true;
+                this._modifiable = true;
+            }
+
+            public Builder(IGraphType type)
+            {
+                this._directed = type.IsDirected || type.IsMixed;
+                this._undirected = type.IsUndirected || type.IsMixed;
+                this._allowSelfloops = type.IsAllowingSelfLoops;
+                this._allowMultipleEdges = type.IsAllowingMultipleEdges;
+                this._weighted = type.IsWeighted;
+                this._allowCycles = type.IsAllowingCycles;
+                this._modifiable = type.IsModifiable;
+            }
+
+            public Builder(bool directed, bool undirected)
+            {
+                if(!directed && !undirected)
+                {
+                    throw new ArgumentException("At least one of directed or undirected must be true");
+                }
+                _directed = directed;
+                _undirected = undirected;
+                _allowSelfloops = true;
+                _allowMultipleEdges = true;
+                _weighted = false;
+                _allowCycles = true;
+                _modifiable = true;
+            }
+
+            public Builder directed()
+            {
+                this._directed = true;
+                this._undirected= false;
+                return this;
+            }
+
+            public Builder undirected()
+            {
+                this._directed = false;
+                this._undirected = true;
+                return this;
+            }
+
+            public Builder mixed()
+            {
+                this._directed = true;
+                this._undirected = false;
+                return this;
+            }
         }
     }
 }
