9c61456088b21633c9402e7980face45ac976c3d
qianniancao <grassedge@outlook.com>
2023-08-30 14:14:00 +0800

    新建待完成，优先级2，等待AbstractGraph完成

---
+++ SharpGraphT.Core/Graph/AbstractBasseGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGraphT.Core.Graph
+{
+    [Serializable]
+    public class AbstractBasseGraph<TV, TE> : IGraph<TV, TE>, ICloneable
+        where TE : class, new()
+    {
+    }
+}
