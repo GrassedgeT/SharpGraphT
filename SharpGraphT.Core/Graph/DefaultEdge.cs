b063840ad6345102dad44f038a715de68f144407
qianniancao <grassedge@outlook.com>
2023-09-01 17:30:43 +0800

    初步完成

---
+++ SharpGraphT.Core/Graph/DefaultEdge.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpGraphT.Core.Graph
+{
+    public class DefaultEdge : IntrusiveEdge
+    {
+        private static readonly long serialVersionUID = 3258408452177932855L;
+
+        protected Object GetSource() => base._source;
+        
+        protected Object GetTarget() => base._target;
+
+        public override String ToString() => $"(source:{GetSource()},target:{GetTarget()})";
+    }
+}
---
+++ SharpGraphT.Core/Graph/IntrusiveEdge.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGraphT.Core.Graph
+{
+    [Serializable]
+    public class IntrusiveEdge : ICloneable
+    {
+        private static readonly long serialVersionUID = 3258408452177932855L;
+
+        protected Object _source;
+
+        protected Object _target;
+
+        public object Clone()
+        {
+            try
+            {
+                return base.MemberwiseClone();
+            }
+            catch (Exception e)
+            {
+                throw new ExecutionEngineException();
+            }
+        }
+    }
+}
