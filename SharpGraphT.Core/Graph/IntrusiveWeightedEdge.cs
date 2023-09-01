e4fd7174833fbe5df7e96d894322a7ce9e43e971
qianniancao <grassedge@outlook.com>
2023-09-01 17:39:20 +0800

    基本完成

---
+++ SharpGraphT.Core/Graph/IntrusiveWeightedEdge.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace SharpGraphT.Core.Graph;
+
+public class IntrusiveWeightedEdge : IntrusiveEdge
+{
+    private static readonly long serialVersionUID = 2890534758523920741L;
+
+    private double weight = IGraph.DefaultEdgeWeight;
+}
