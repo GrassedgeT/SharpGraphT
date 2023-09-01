94b9f39a38cd2fc6b873899fc0cbb59f2b5b2587
qianniancao <grassedge@outlook.com>
2023-09-01 17:44:55 +0800

    基本完成

---
+++ SharpGraphT.Core/Graph/DefaultWeightedEdge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGraphT.Core.Graph
+{
+    public class DefaultWeightedEdge : IntrusiveWeightedEdge
+    {
+        private static readonly long serialVersionUID = -3259071493169286685L;
+
+        protected Object GetSource() => _source;
+
+        protected Object GetTarget() => _target;
+
+        protected double GetWeight() => Weight;
+
+        public override String ToString() => $"(source:{GetSource()},target:{GetTarget()},weight:{GetWeight()})";
+    }
+}
