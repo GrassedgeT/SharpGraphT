7fe076fbd0e8b00f67987768021473bad7c9ca97
qianniancao <1962400688@qq.com>
2023-09-02 20:51:08 +0800

    初步完成

---
+++ SharpGraphT.Core/Graph/specifics/ArrayUnenforcedSetEdgeSetFactory.cs
@@ -0,0 +1,14 @@
+﻿using SharpGraphT.Core.Graph;
+using SharpGraphT.Util;
+
+namespace SharpGraphT.Core.Graph.specifics;
+[Serializable]
+public class ArrayUnenforcedSetEdgeSetFactory<TV, TE> : IEdgeSetFactory<TV, TE>
+    where TE : class, new()
+{
+    private static readonly long serialVersionUID = 5936902837403445985L;
+
+    public ISet<TE> CreateEdgeSet(TV vertex) => new ArrayUnenforcedSet<TE>(1);
+    
+}
+    
