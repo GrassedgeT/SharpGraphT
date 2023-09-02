6d3f23857f889ac87cab0d6f58706be9d5bb9ade
qianniancao <1962400688@qq.com>
2023-09-02 20:54:09 +0800

    初步完成

---
+++ SharpGraphT.Core/Graph/IGraphSpecificsStrategy.cs
@@ -0,0 +1,12 @@
+﻿using SharpGraphT.Core.Graph;
+using SharpGraphT.Core.Graph.specifics;
+
+namespace SharpGraphT.Core;
+
+public interface IGraphSpecificsStrateg<TV, TE>
+    where TE : class, new()
+{
+    Func<IGraphType, IIntrusiveEdgesSpecifics<TV, TE>> GetIntrusiveEdgesSpecifics();
+    Func<IGraph<TV, TE>, IGraphType, ISpecifics<TV, TE>> GetSpecificsFactory();
+    IEdgeSetFactory<TV, TE> GetEdgeSetFactory() => new ArrayUnenforcedSetEdgeSetFactory<TV, TE>();
+}
