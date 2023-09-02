87ac5391fb6ea42935a9de360608c4f3c47479fe
qianniancao <1962400688@qq.com>
2023-09-02 15:09:55 +0800

    初步完成

---
+++ SharpGraphT.Core/Graph/IIntrusiveEdgesSpecifics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SharpGraphT.Core.Graph;
+
+public interface IIntrusiveEdgesSpecifics<TV, TE>
+    where TE : class, new()
+{
+    TV GetEdgeSource(TE e);
+    TV GetEdgeTarget(TE e);
+    bool Add(TE e, TV sourceVertex, TV targetVertex);
+    bool ContainsEdge(TE e);
+    IEnumerable<TE> GetEdgesSet();
+    void Remove(TE e);
+    double GetEdgeWeight(TE e);
+    void SetEdgeWeight(TE e, double weight);
+}
