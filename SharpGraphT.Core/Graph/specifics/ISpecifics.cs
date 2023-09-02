209fcf68342a6ac83f0cf8aed8f9f12d36306be5
qianniancao <1962400688@qq.com>
2023-09-02 16:43:41 +0800

    修改错误

--- SharpGraphT.Core/Graph/specifics/ISpecifics.cs
+++ SharpGraphT.Core/Graph/specifics/ISpecifics.cs
@@ -4,18 +4,18 @@ public interface ISpecifics<TV, TE>
     where TE : class, new()
 {
     bool AddVertex(TV vertex);
-    IEnumerable<TV> GetVertexSet();
-    IEnumerable<TE> GetAllEdges(TV sourceVertex, TV targetVertex);
+    IReadOnlySet<TV> GetVertexSet();
+    IReadOnlySet<TE> GetAllEdges(TV sourceVertex, TV targetVertex);
     TE GetEdge(TV sourceVertex, TV targetVertex);
     bool AddEdgeToTouchingVertices(TE edge, TV sourceVertex, TV targetVertex);
     bool AddEdgeToTouchingVerticesIfAbsent(TV sourceVertex, TV targetVertex, TE edge);
     TE CreateEdgeToTouchingVerticesIfAbsent(TV sourceVertex, TV targetVertex, Func<TV, TV, TE> edgeSupplier);
     int DegreeOf(TV vertex);
-    IEnumerable<TE> EdgesOf(TV vertex);
+    IReadOnlySet<TE> EdgesOf(TV vertex);
     int InDegreeOf(TV vertex);
-    IEnumerable<TE> IncomingEdgesOf(TV vertex);
+    IReadOnlySet<TE> IncomingEdgesOf(TV vertex);
     int OutDegreeOf(TV vertex);
-    IEnumerable<TE> OutgoingEdgesOf(TV vertex);
+    IReadOnlySet<TE> OutgoingEdgesOf(TV vertex);
     void RemoveEdgeFromTouchingVertices(TE edge, TV sourceVertex, TV targetVertex, TE e);
 }
 
